660e3c0d1e4492cdb4962072cfcbaaf66cd6a06c
Jean Claro <jeanclarodev@gmail.com>
2024-05-13 23:53:32 -0300

    finished

--- js/scriptdonate.js
+++ js/scriptdonate.js
@@ -1,6 +1,29 @@
 let buttonDonate1 =  document.getElementById('optionDonate1')
 let buttonDonate2 =  document.getElementById('optionDonate2')
 
+var cpfFormatado = false;
+
+function addnewCPF() {
+    var CPF = document.getElementById("labelcpf").value;
+    var newCPF = "";
+    
+     if(CPF.length !== 11 && cpfFormatado === true) {
+            cpfFormatado = false;
+            CPF = ""
+        }; 
+        if (!cpfFormatado && CPF.length > 10) {
+        newCPF = CPF.slice(0, 3) + "." + CPF.slice(3, 6) + "." + CPF.slice(6, 9) + "-" + CPF.slice(9);
+        cpfFormatado = true; 
+        } else {
+            newCPF = CPF; 
+                }
+    
+    document.getElementById("labelcpf").value = newCPF;
+}
+$(document).ready(function(){
+    $('#labeldate').mask('00/00/0000');
+});
+
 let button1Selected = false;
 buttonDonate1.addEventListener("click", function(event) {
     button1Selected = true
@@ -23,9 +46,6 @@ buttonDonate2.addEventListener("click", function(event) {
 function parameter(button1Selected, button2Selected){
     button1ConfirmSelected(button1Selected, button2Selected)
 }
-
-
-
 
 
 
